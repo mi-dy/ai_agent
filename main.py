149a47991c4804fe3e0a254528382f13a33106ff
mi-dy <mindaugasdy@gmail.com>
2026-03-06 11:40:02 +0200

    User prompt integration

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 
@@ -9,11 +10,18 @@ def main():
     if api_key == None:
         raise RuntimeError("Gemini api key not found")
 
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
             model="gemini-2.5-flash",
-            contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+            contents=args.user_prompt
             )
+    
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(response.text)
 
 
