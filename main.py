9df32e87ee28704f0ba968ef0fc23123f2b8c70c
mi-dy <mindaugasdy@gmail.com>
2026-03-08 18:00:15 +0200

    Added messages logging

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
@@ -14,11 +15,14 @@ def main():
     parser.add_argument("user_prompt", type=str, help="User prompt")
     args = parser.parse_args()
 
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
             model="gemini-2.5-flash",
-            contents=args.user_prompt
+            contents=messages,
             )
+
     
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
